18170d898159263a6dbc3945c46aae22821b8597
Daniel Andersen <daniel.andersen@alexandra.dk>
2016-03-18 08:33:35 +0100

    More tests. One failing.

--- Interactive Tabletop Projected/src/board/test.py
+++ Interactive Tabletop Projected/src/board/test.py
@@ -15,6 +15,20 @@ def run_tests():
     brick_detector = TileBrickDetector()
 
     test_set = {
+        "board/training/practice1a.png": [
+            (None, [(2, 15), (2, 16), (2, 17), (3, 17), (4, 17)]),
+            (None, [(3, 4), (3, 3), (3, 2), (3, 1), (4, 1)]),
+            (None, [(4, 10), (5, 10), (5, 11), (5, 12), (5, 13), (6, 12)]),
+            (None, [(10, 1), (10, 2), (10, 3), (11, 3), (12, 3), (10, 4), (10, 5)]),
+            (None, [(14, 14), (14, 15), (12, 16), (13, 16), (14, 16), (15, 16), (16, 16)]),
+            (None, [(16, 1), (17, 1), (18, 1), (19, 1), (20, 1), (18, 2), (18, 3)]),
+            (None, [(18, 19), (17, 18), (18, 18), (19, 18)]),
+            (None, [(23, 14), (22, 14), (24, 14), (23, 13), (23, 15)]),
+            (None, [(29, 3), (29, 4), (29, 5), (29, 6), (29, 7), (28, 6)]),
+            (None, [(30, 11), (30, 12), (30, 13), (29, 13), (29, 14)]),
+            (None, [(16, 12), (15, 12), (16, 12), (17, 12), (18, 12)]),
+            (None, [(24, 14), (25, 14), (26, 14), (27, 14)]),
+        ],
         "board/training/practice1b.png": [
             ((2, 17), [(2, 15), (2, 16), (2, 17), (3, 17), (4, 17)]),
             ((3, 2), [(3, 4), (3, 3), (3, 2), (3, 1), (4, 1)]),
@@ -49,7 +63,7 @@ def run_tests():
         for brick_position, tile_array in tests:
             tile = brick_detector.find_brick_among_tiles(descriptor, tile_array)
             if tile != brick_position:
-                print("Test failed. Brick supposed to be at: %s but was at: %s" % (brick_position, tile))
+                print("Test failed. Brick supposed to be at: %s but was at: %s. Image: %s" % (brick_position, tile, image_filename))
                 failed += 1
                 continue
             passed += 1
