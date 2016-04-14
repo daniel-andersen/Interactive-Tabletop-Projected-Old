1e7e13b932046fa092657e7ed7b6dea0e80e4200
Daniel Andersen <daniel.andersen@alexandra.dk>
2016-04-14 18:30:04 +0200

    Output images

--- Server/src/server/reporters/tiled_brick_position_reporter.py
+++ Server/src/server/reporters/tiled_brick_position_reporter.py
@@ -30,9 +30,11 @@ class TiledBrickPositionReporter(Reporter):
             self.board_descriptor.snapshot = self.board_recognizer.find_board(image, self.board_descriptor)
 
             if self.board_descriptor.is_recognized():
-                cv2.imwrite("output1.png", self.board_descriptor.snapshot.board_image)
+                cv2.imwrite("output_board_recognized.png", self.board_descriptor.snapshot.board_image)
                 tile = self.tile_brick_detector.find_brick_among_tiles(self.board_descriptor, self.valid_locations)
                 if tile is not None:
-                    cv2.imwrite("output2.png", image)
+                    cv2.imwrite("output_brick_recognized.png", image)
                     self.callback_function(tile)
                     self.stop()
+            else:
+                cv2.imwrite("output_board_not_recognized.png", self.board_descriptor.snapshot.board_image)
