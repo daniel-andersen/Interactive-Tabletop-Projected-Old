c112a5d1be52189cead2c37c18c0638d059ecc80
Daniel Andersen <daniel.andersen@alexandra.dk>
2016-06-15 10:35:14 +0200

    Cloned simple websocket server

--- Server/install_packages.sh
+++ Server/install_packages.sh
@@ -2,4 +2,4 @@
 
 sudo pip install numpy
 sudo pip install "picamera[array]"
-sudo pip install git+https://github.com/dpallot/simple-websocket-server.git
+sudo pip install git+https://github.com/daniel-andersen/simple-websocket-server.git
